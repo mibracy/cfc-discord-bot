c6b5f99c965aceba5ecea05e890105f1a400d214
Michael Bracy <crusty512@yahoo.com>
2023-02-04 19:10:38 +0000

    Burke's Dance Moves

--- commands/leaderboard.js
+++ commands/leaderboard.js
@@ -3,8 +3,17 @@ const { SlashCommandBuilder } = require('discord.js');
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName(`leaderboard`)
-		.setDescription(`THE tripple threat -straight white man- most hated study helper`),
+		.setDescription(`THE tripple threat -straight white man- most hated study helper`)        
+		.addBooleanOption(option =>
+            option.setName('dance')
+			    .setDescription('Who Can Dance If He Wants To?')
+                .setRequired(true)),
 	async execute(interaction) {
-		await interaction.reply({content: `https://cdn.discordapp.com/attachments/972712505417801728/1070213679779426314/Screenshot_2023-01-31_at_11.26.54_PM.png`, ephemeral: true });
+		const dance = interaction.options.getBoolean(`alt`);
+		if (dance === true){
+			await interaction.reply({content: `https://cdn.discordapp.com/attachments/962864335074496562/1063678344362729603/burkieDance.mp4`, ephemeral: true });
+		} else {
+			await interaction.reply({content: `https://cdn.discordapp.com/attachments/972712505417801728/1070213679779426314/Screenshot_2023-01-31_at_11.26.54_PM.png`, ephemeral: true });
+		}
 	},
 };
